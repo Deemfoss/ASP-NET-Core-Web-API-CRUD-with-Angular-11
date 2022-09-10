11ac9c294db5571d9464c96abcd8fbfeedd3637f
Gaogao Han <firstName.lastName@corp.trizetto.com>
2022-09-10 15:42:41 -0400

    Add Models and Context into Web API

---
+++ PaymentAPI/PaymentAPI_1/Models/PaymentDetialContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetialContext : DbContext
+    {
+        public PaymentDetialContext(DbContextOptions<PaymentDetialContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
